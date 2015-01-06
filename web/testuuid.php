ea6ccf1aaa465f89bd1feed82ccbf8afbf468cc3
Willem-Jan <wjzijderveld@gmail.com>
2015-01-06 10:27:39 +0100

    Removed file that shouldn't be there

--- web/testuuid.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-try {
-var_dump(Assert\Assertion::uuid('89334a7d-cf37-4c5b-acf1-0f39a19c5547'));
-} catch (\Exception $e) {
-var_dump($e);
-}
