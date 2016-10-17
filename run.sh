7b0838ed0825f0ede65b709ae8184140d6d16faa
Gaultier Boniface <gboniface@wysow.fr>
2016-10-17 09:42:16 +0200

    optimized docker setup
    * use docker-compose
    
    * Optimize the whole setup to work correctly in docker.
    
    * Rebase and optimize run script.
    
    * Optimize composer autoload configuration and delete useless paragraph in README.

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # install dependencies
-composer install --ignore-platform-reqs # ignore required mongo PHP extension as it is not used in this application
+composer install
+
+# clear and warmup app cache
+app/console cache:clear --env=prod
+app/console cache:warmup --env=prod
 
 # initialize the database
-app/console broadway:event-store:schema:init
+app/console broadway:event-store:schema:init --env=prod
 
 # run the app
-app/console server:run -vvv --env=prod 0.0.0.0:8000
+app/console server:run --env=prod -vvv 0.0.0.0:8000
