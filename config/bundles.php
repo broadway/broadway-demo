590536003842cef7768f56bff9400b1dbe4c7fef
othillo <othillo@othillo.nl>
2021-07-06 11:44:52 +0200

    require doctrine-bundle ^2.0

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
